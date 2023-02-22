eafaa1185a8615d325c449fb95725896754bce69
Vladimir Ya <nemoepalto@gmail.com>
2023-02-22 13:03:46 +0300

    v1.0 Добавил поддержку чисел с плавающей точкой

--- equation.cpp
+++ equation.cpp
@@ -3,12 +3,11 @@
 #include <algorithm>
 #include <cassert>
 #include <iostream>
-#include <optional>
 #include <stdexcept>
 
 
 equation::equation(std::string s)
-  : valid_symbols_{std::string{"1234567890"} + "+-*/" + "=x"} // TODO: Можно добавить X в верх. регистре
+  : valid_symbols_{std::string{"1234567890"} + "+-*/" + '.' + "=x"} // TODO: Можно добавить X в верх. регистре
   , equation_str_{s}
   , key_pos_{{'x', std::string::npos},
              {'=', std::string::npos}} {
@@ -87,10 +86,7 @@ equation::equation(std::string s)
     if (it != vs_cend) {
       throw std::runtime_error(std::string{"Error #10: Bad symbol \""} + symb_after_x + "\" after \"x\"");
     }
-  } else {
-    throw std::runtime_error(std::string{"WARNING / Error #...: Now \"x\" can't be the last symbol"});
   }
-
 
   // Проверка отсутствия дублирования операторов, например:
   //  2-*x=4   // Error
@@ -107,49 +103,44 @@ equation::equation(std::string s)
       prev_symb.reset();
     }
   }
-
-  // Пока игнорирую подобные типы уравнений
-  double y1 = 2* - 1;
-  double y2 = 2.0* - 1.0 / 10;
 }
 
 
 double equation::get_x() {
   coefficients coefs = parse(key_pos_);
 
-  // TODO: Предполагается, что b_before_kx и b_after_kx - числа (например, -7),
-  // а не последовательность вычислений (например, 7*4/2-9).
-  std::map<std::string, std::string> b_before_and_after_kx{{coefs.b_before_kx, "b_before_kx= "},
-                                                           {coefs.b_after_kx,  "b_after_kx= "}};
-  for (const auto& [b, str] : b_before_and_after_kx) {
-    auto it = std::find_if(b.cbegin(), b.cend(), [](const char s) {
-      return s == '*' ||
-             s == '/' ||
-             s == '-' ||
-             s == '+';
-    });
-    if (it != b.cend()) {
-      if (it == b.cbegin() && (*it == '+' || *it == '-')) {
-        continue;
+  {
+    // Предполагается, что b_before_kx, b_after_kx и y - числа (например, -7),
+    // а не последовательность вычислений (например, 7*4/2-9).
+    std::vector<std::pair<std::string, std::string>> basic_values{{coefs.b_before_kx, "b_before_kx = "},
+                                                                  {coefs.b_after_kx,  "b_after_kx = "},
+                                                                  {coefs.y,           "y = "}};
+    for (const auto& [val, str] : basic_values) {
+      for (auto it = val.cbegin(); it != val.cend(); ++it) {
+        if (*it == '*' || *it == '/' || *it == '-' || *it == '+') {
+          if (it == val.cbegin() && (*it == '+' || *it == '-')) {
+            continue;
+          } else {
+            throw std::runtime_error(std::string{"Error #13: Ожидается, что "} + str + val +
+                                     " является числом, а не выражением");
+          }
+        }
       }
-      throw std::runtime_error(std::string{"Error #15: Ожидается, что "} +
-                               str + b + " является числом, а не выражением");
     }
   }
 
+  check_num_syntax(coefs.b_before_kx);
   double b_bef = coefs.b_before_kx.empty() ? 0.0 : std::stod(coefs.b_before_kx);
 
+  check_num_syntax(coefs.b_after_kx);
   double b_aft = coefs.b_after_kx.empty() ? 0.0
-                 : coefs.operator_after_kx == '-' ? std::stod(coefs.b_after_kx) * (- 1)
-                                                  : std::stod(coefs.b_after_kx);
+                                          : coefs.operator_after_kx == '-' ? std::stod(coefs.b_after_kx) * (- 1)
+                                                                           : std::stod(coefs.b_after_kx);
+  check_num_syntax(coefs.y);
   double rhs_value = std::stod(coefs.y) - b_bef - b_aft;
 
-  std::cout << "b_bef = " << b_bef << " and " << b_aft << std::endl;
-  std::cout << "rhs_value AT START: " << rhs_value << std::endl;
-
   // После предыдущих проверок предполагаем, что наше уравнение корректно
-
-  auto x_pos = coefs.kx.find('x'); // Точно есть, проверка не требуется
+  auto x_pos = coefs.kx.find('x');
   assert(x_pos != std::string::npos);
 
   parse_expression_after_x(coefs.kx.substr(x_pos), rhs_value);
@@ -168,7 +159,7 @@ double equation::get_x() {
       }
     } else if (op == '/') {
       if (rhs_value == 0) {
-        throw std::runtime_error(std::string{"Error #14: Уравнение не имеет корней. В процессе решения уравнения возникает операция деления на 0"});
+        throw std::runtime_error(std::string{"Error #16: Уравнение не имеет корней. В процессе решения уравнения возникает операция деления на 0"});
       }
       x = expr_before_x.value() / rhs_value;
       if (coefs.operator_before_kx == '-') {
@@ -184,20 +175,19 @@ double equation::get_x() {
     x = rhs_value;
   }
 
+//  auto print = [](const std::string& s, bool wh_sp = true) {
+//    std::cout << (s.empty() ? " " : s) << (wh_sp ? "__" : "");
+//  };
 
-  auto print = [](const std::string& s, bool wh_sp = true) {
-    std::cout << (s.empty() || (s.size() == 1 && s.at(0) == 0x00) ? " " : s) << (wh_sp ? "__" : "");
-  };
-
-  std::cout << "Результат разбора: \n";
-  print(coefs.b_before_kx);
-  print({coefs.operator_before_kx});
-  print(coefs.kx);
-  print({coefs.operator_after_kx});
-  print(coefs.b_after_kx);
-  print(" = ");
-  print(coefs.y, false);
-  std::cout << std::endl;
+//  std::cout << "Результат разбора: \n";
+//  print(coefs.b_before_kx);
+//  print({coefs.operator_before_kx.value_or(' ')});
+//  print(coefs.kx);
+//  print({coefs.operator_after_kx.value_or(' ')});
+//  print(coefs.b_after_kx);
+//  print(" = ");
+//  print(coefs.y, false);
+//  std::cout << std::endl;
 
   return x;
 }
@@ -234,7 +224,7 @@ coefficients equation::parse(const std::map<char, std::size_t>& key_pos) {
     coefs.kx = equation_str_.substr(left_kx_border - equation_str_.cbegin() + ident,
                                     right_kx_border - left_kx_border - ident);
   } else {
-    throw std::runtime_error(std::string{"Error #14: В уравнении \"x\" расположен после \"=\""});
+    throw std::runtime_error(std::string{"Error #12: В уравнении \"x\" расположен после \"=\""});
   }
 
   return coefs;
@@ -257,35 +247,27 @@ void equation::parse_expression_after_x(std::string expr, double& rhs_value) {
       assert(num_len > 0); // На основании прошлых проверок
 
       double num = 0;
-      try {
-        int pos = expr.crend() - it; // - it_from - num_len;
-//        std::cout << "pos = " << pos << ", len = " << num_len;
-//        std::cout << ", val = " << expr.substr(pos, num_len) << std::endl;
-        num = std::stod(expr.substr(pos, num_len));
-      } catch (...) {
-        std::cout << "Error #1000: Internal logic error - bad arg at std::stod()" << std::endl;
-        std::terminate();
-      }
+      int pos = expr.crend() - it; // - it_from - num_len;
+      auto num_str = expr.substr(pos, num_len);
+
+      check_num_syntax(num_str);
+      num = std::stod(num_str);
 
       if (*it == '*') {
         if (num == 0) {
-          throw std::runtime_error(std::string{"Error #12: Уравнение не имеет корней, т.к. один из множителей \"x\" равен 0"});
+          throw std::runtime_error(std::string{"Error #14.1: Уравнение не имеет корней, т.к. один из множителей \"x\" равен 0"});
         }
         rhs_value = rhs_value / num;
 
       } else if (*it == '/') {
         if (num == 0) {
-          throw std::runtime_error(std::string{"Error #13: В уравнении присутствует операция деления на 0"});
+          throw std::runtime_error(std::string{"Error #15.1: В уравнении присутствует операция деления на 0"});
         }
         rhs_value = rhs_value * num;
 
       } else {
         assert(false);
       }
-
-
-      auto num_as_str = expr.substr(expr.size() - num_len, num_len);
-      std::cout << num_len << " and " << num_as_str << " rhs = " << rhs_value << std::endl;
 
       it_from = ++it;
     } else {
@@ -323,15 +305,11 @@ std::optional<double> equation::calc_expression_before_x(std::string expr) {
       assert(num_len > 0); // На основании прошлых проверок
 
       double num = 0;
-      try {
-        std::size_t pos = it_from - expr.cbegin();
-//        std::cout << "pos = " << pos << ", len = " << num_len;
-//        std::cout << ", val = " << expr.substr(pos, num_len) << std::endl;
-        num = std::stod(expr.substr(pos, num_len));
-      } catch (...) {
-        std::cout << "Error #1000: Internal logic error - bad arg at std::stod()" << std::endl;
-        std::terminate();
-      }
+      std::size_t pos = it_from - expr.cbegin();
+      auto num_str = expr.substr(pos, num_len);
+
+      check_num_syntax(num_str);
+      num = std::stod(num_str);
 
       if (!res.has_value()) {
         res = num;
@@ -340,13 +318,13 @@ std::optional<double> equation::calc_expression_before_x(std::string expr) {
       } else {
         if (*op.value() == '*') {
           if (num == 0) {
-            throw std::runtime_error(std::string{"Error #12.2: Уравнение не имеет корней, т.к. один из множителей \"x\" равен 0"});
+            throw std::runtime_error(std::string{"Error #14.2: Уравнение не имеет корней, т.к. один из множителей \"x\" равен 0"});
           }
           res = res.value() * num;
 
         } else if (*op.value() == '/') {
           if (num == 0) {
-            throw std::runtime_error(std::string{"Error #13.2: В уравнении присутствует операция деления на 0"});
+            throw std::runtime_error(std::string{"Error #15.2: В уравнении присутствует операция деления на 0"});
           }
           res = res.value() / num;
 
@@ -362,4 +340,19 @@ std::optional<double> equation::calc_expression_before_x(std::string expr) {
   }
 
   return res;
+}
+
+
+void equation::check_num_syntax(const std::string &value) const {
+  if (value.empty()) {
+    return;
+  }
+
+  std::size_t amount = std::count(value.cbegin(), value.cend(), '.');
+  if (amount > 1 ||
+      value.back() == '.' ||
+      value.front() == '.') {
+    throw std::runtime_error(std::string{"Error #17: Последовательность символов "} + value +
+                             " не является числом или содержит лишние символы '.'");
+  }
 }
--- equation.h
+++ equation.h
@@ -1,17 +1,18 @@
 #pragma once
 
 #include <map>
+#include <optional>
 #include <string>
 
 
-// Коэффициенты уравнения b_bef + kx + b_aft = y
+// Части уравнения b_bef + kx + b_aft = y
 struct coefficients {
-  std::string y;
-  std::string b_before_kx;
-  char operator_before_kx = 0x00; // Заменить char на enum'ы
-  std::string kx;
-  char operator_after_kx = 0x00; // Заменить char на enum'ы
-  std::string b_after_kx;
+  std::string         b_before_kx;
+  std::optional<char> operator_before_kx;
+  std::string         kx;
+  std::optional<char> operator_after_kx;
+  std::string         b_after_kx;
+  std::string         y;
 };
 
 
@@ -24,6 +25,7 @@ private:
   coefficients parse(const std::map<char, std::size_t>& key_pos);
   void parse_expression_after_x(std::string expr, double &rhs_value);
   std::optional<double> calc_expression_before_x(std::string expr);
+  void check_num_syntax(const std::string& value) const;
 
 private:
   const std::string           valid_symbols_;
